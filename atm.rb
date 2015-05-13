f92cc2db1db38762ed5070d335cd7ee6de8f25b9
shaila <shaila.rcabrera@gmail.com>
2015-05-13 20:51:14 +0200

    Add test methods

--- atm.rb
+++ atm.rb
@@ -8,6 +8,40 @@ class Atm
   def run
     balance = STARTING_BALANCE
 
+    print_balance(balance)
+
+    puts"What doy want to do? D, W, B or Q #{option_user}"
+    option.gets.chomp
+  end
+
+  def print_balance(balance)
     printf "Your balance is $%.2f\n", balance
+  end
+
+
+
+  def test
+    a = 5
+    b = 10.0
+    result = product(a, b)
+    puts "Result is #{result}"
+    array = [1, 2, 3, 4]
+    #sum(array)
+    puts "Sum is #{sum(array)}"
+
+    run
+  end
+
+  def sum(list)
+    res = 0
+    list.each do |elem|
+      res += elem
+    end
+
+    res
+  end
+
+  def product(x, y)
+    x*y
   end
 end
